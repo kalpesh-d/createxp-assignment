e958d4e01e654ced85c4b931eda8c653becf605b
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2025-02-12 16:20:10 +0530

    Comment out unused Jobs interface and API fetch logic in JobsPage component

--- app/candidate/jobs/page.tsx
+++ app/candidate/jobs/page.tsx
@@ -1,27 +1,27 @@
-interface Jobs {
-  id: string;
-  title: string;
-  description: string;
-  category: string;
-  location: string;
-  salaryMin?: number;
-  salaryMax?: number;
-  type: string;
-  requirements?: string;
-  companyId: string;
-}
+// interface Jobs {
+//   id: string;
+//   title: string;
+//   description: string;
+//   category: string;
+//   location: string;
+//   salaryMin?: number;
+//   salaryMax?: number;
+//   type: string;
+//   requirements?: string;
+//   companyId: string;
+// }
 
 const JobsPage = async () => {
-  const BaseUrl = 'http://localhost:3000/api/jobs';
+  // const BaseUrl = 'http://localhost:3000/api/jobs';
 
-  const res = await fetch(BaseUrl)
-  const data: Jobs[] = await res.json(); 
+  // const res = await fetch(BaseUrl)
+  // const data: Jobs[] = await res.json(); 
 
   return (
     <ul>
-      {data.map(job => (
+      {/* {data.map(job => (
         <li key={job.id}>{job.title}</li>
-      ))}
+      ))} */}
     </ul>
   )
 }
